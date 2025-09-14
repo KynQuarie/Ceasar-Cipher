3f10416c9e15812c8b64b9d23fbe65e875999754
Kyn Quarie Infante <kyn.infante10@gmail.com>
2025-09-14 11:28:12 +0800

    function testing 2.5

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def greet(name):
-    print(f"Hellow, {name}")
-    print(f"how are you, {name}?")
-    print(f"How is the weather, {name}?")
+#Test 1
+# def greet(name):
+#     print(f"Hellow, {name}")
+#     print(f"how are you, {name}?")
+#     print(f"How is the weather, {name}?")
+# greet("Kyn Quarie")
 
-greet("Kyn Quarie")
+#Test 2
+# def life_in_weeks(age):
+#     left_year = 90 - age
+#     left_week = 52 * left_year
+#     print(f"You have {left_week} weeks left.")
+# life_in_weeks(23)
+
